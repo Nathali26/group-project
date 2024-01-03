fa5270a212f445caed5a69263b61805750b75eeb
Nathalia Rojas Cano <nathaliarc0826@gmail.com>
2024-01-03 15:26:32 +0100

    Initial Express commit

---
+++ model/init_db.sql
@@ -0,0 +1,10 @@
+
+-- Creación de tablas para un proyecto de recetas
+--
+
+-- Desactivar verificación de claves foráneas temporalmente
+SET foreign_key_checks = 0;
+
+
+-- Reactivar verificación de claves foráneas
+SET foreign_key_checks = 1;
