8974e8501cbb0265c71574a1311e5ddde9ece6c8
Ioanna Papaioannou <ipapioani@gmail.com>
2024-01-22 18:47:14 +0100

    commit Flights.jsx

--- client/src/components/Flights.jsx
+++ client/src/components/Flights.jsx
@@ -2,47 +2,52 @@ import React from 'react';
 import { useState } from 'react';
 
 export default function Flights({onSearch}) {
-  const [airportCode, setairportCode] = useState('');
-  const [results, setResults]= useState([])
+  const [cityName, setCityName] = useState('');
+  const [airportCode, setAirportCode]= useState([])
+  const [error, setError] = useState('');   /* a state to save all of the errors */
+  const [isLoading , setIsloading] = useState(false)
 
   const handleChange = (event)=>{
     const value = event.target.value;
-    setairportCode(value)
+    setCityName(value)
 
   };
 const handleSubmit = (event)=>{
   event.preventDefault();
-  searchFlights();
-  onSearch(airportCode);
-  setairportCode('');
+  searchAirport();
+  onSearch(cityName);
+  setCityName('');
 }
 
-const url = 'https://tripadvisor16.p.rapidapi.com/api/v1/flights/searchFlights';
+const url = 'https://tripadvisor16.p.rapidapi.com/api/v1/flights/searchFlights';    /* ??????? */
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '693a320c7amshf8a3f0479327cbap12dca4jsn254f4f98016a',
 		'X-RapidAPI-Host': 'tripadvisor16.p.rapidapi.com'
 	},
-  params: {query: 'Athens'},
+  params: {query: 'Athens'},   /* ???????? */
 };
-async function searchFlights() {
+async function searchAirport(name) {
+  setIsloading(true)
+    setAirportCode(null)
+    setError('');
   try {
-    const response = await fetch(url, options);
+    const response = await fetch(url, options);    /* ?????? */
     const result = await response.text();
-    console.log(result);
-    setResults(result);
+    console.log(airportCode);
+    setAirportCode(airportCode);
   } catch (error) {
     console.error(error);
   } }
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="airport">City</label>
+      <label htmlFor="airport">Where from</label>
       <input id="airport"
               type='text'
-              placeholder='city'
-              value = {city}
+              placeholder='Type city name...'
+              value = {airportCode}
               onChange = {handleChange} />
       <button type='submit'>Search</button>
     </form>
