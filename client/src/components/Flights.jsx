1821e8cd400b977564b9bdce2d6223c2426f25f1
Sarah Wilson <wilson.e.sarah@gmail.com>
2024-01-25 14:53:46 +0100

    styling and routes

--- client/src/components/Flights.jsx
+++ client/src/components/Flights.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useEffect } from 'react';
+import './Flights.css';
 
 export default function Flights() {
   
     //I set the backgrounds here because I was having trouble setting different backgrounds for different components
     useEffect(() => {
       // Set the background image when the component mounts
-      document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1592462131333-62cacb894b9e?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")';
+      document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1606768666853-403c90a981ad?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")';
       document.body.style.backgroundSize = 'cover';
       document.body.style.backgroundAttachment = 'fixed';
   
