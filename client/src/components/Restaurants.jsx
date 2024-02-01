cdcc61fd2e1bae405115f478b44253b4b207eb9d
Sarah Wilson <wilson.e.sarah@gmail.com>
2024-02-01 16:07:54 +0100

    matched my Restaurants file with main

--- client/src/components/Restaurants.jsx
+++ client/src/components/Restaurants.jsx
@@ -3,20 +3,14 @@ import axios from "axios";
 import './Restaurants.css';
 
 export default function Restaurants() {
-
   const [restaurants, setRestaurants] = useState([]);
-  const [consult, setConsult] = useState("");
-
-  const [checkIn, setCheckIn] = useState("");
-  const [checkOut, setCheckOut] = useState("");
+  const [consult, setConsult] = useState('');
+  const [book, setBook] = useState("");
 
   useEffect(() => {
-    // Set the background image when the component mounts
-    document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")';
+    document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=2070&auto=format&f[…]3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")';
     document.body.style.backgroundSize = 'cover';
-    document.body.style.backgroundAttachment = 'fixed';
-
-    // Cleanup function to reset the background when the component unmounts
+    document.body.style.backgroundAttachment = 'fixed'
     return () => {
       document.body.style.backgroundImage = '';
       document.body.style.backgroundSize = '';
@@ -24,66 +18,82 @@ export default function Restaurants() {
     };
   }, []);
 
+  const handleChange = (e) => {
+    setConsult(e.target.value)
+    console.log(consult)
+  }
+
   const handleSearchRestaurants = async () => {
-    try {
-      // Aquí convertimos las fechas al formato YYYY-MM-DD
-      const formattedCheckIn = new Date(checkIn).toISOString().split("T")[0];
-      const formattedCheckOut = new Date(checkOut).toISOString().split("T")[0];
-
-
-      const response = await axios.get("/api/searchLocation", {
-  params: {
-    query: consult,
-    checkIn: formattedCheckIn,
-    checkOut: formattedCheckOut,
+    // try {
+      const options = {
+        method: 'GET',
+        url: "https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchLocation",
+        params: {
+          query: consult
         },
-      });
-      // Log the complete response for debugging
-      console.log("Complete API Response:", response);
-
-      // Verifica si la respuesta y la estructura de datos son las esperadas
-      if (
-        response.data &&
-        response.data.data &&
-        Array.isArray(response.data.data)
-      ) {
-        // Extract data from the response
-        const restaurantData = response.data.data;
-
-        // Verifica si hotelData tiene la estructura correcta
-        if (Array.isArray(hotelData)) {
-          // Extract relevant information for each hotel
-          const formattedHotels = restaurantData.map((hotel) => ({
-            id: restaurant.id,
-            title: restaurant.title,
-            rating: restaurant.bubbleRating?.rating || null,
-            provider: restaurant.provider,
-            price: restaurant.priceForDisplay?.text || null,
-            originalPrice: restaurant.strikethroughPrice?.text || null,
-            externalUrl: restaurant.commerceInfo?.externalUrl || null,
-          }));
-
-          // Update state with the formatted hotel data
-          setHotels(formattedHotels);
-          // Log the complete hotel response for debugging
-          console.log("Complete Restaurant Response:", response);
-
-          // Log the extracted hotel details for debugging
-          console.log("Restaurant Details:", formattedRestaurants);
-        } else {
-          console.error("Invalid hotelData structure:", restaurantData);
+        headers: {
+          'X-RapidAPI-Key': 'e7ad36b881msh8e2a70e78dfdfe8p16ef80jsn7279e8c06fa7',
+          'X-RapidAPI-Host': 'tripadvisor16.p.rapidapi.com'
         }
-      } else {
-        console.error("Invalid response structure:", response.data);
       }
-    } catch (error) {
-      console.error("Error", error);
-    }
-  };
+    // }
+  // } else {
+  //   console.error("Invalid response structure:", response.data);
+  // }
+  
 
-  useEffect(() => {}, [restaurants]);
 
+      const response = await axios.request(options);
+      console.log("i got here");
+      console.log(response);
+    
+      // if (response.data && response.data.data && Array.isArray(response.data.data)) {
+       const restaurantData = response.data.data[0];
+       console.log(restaurantData.locationId);
+
+       let locationId = restaurantData.locationId
+         
+
+      //  try {
+        const restaurantResponse = {
+          method: 'GET',
+          url: "https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants",
+          params: {
+            locationId: locationId
+          },
+          headers: {
+            'X-RapidAPI-Key': 'e7ad36b881msh8e2a70e78dfdfe8p16ef80jsn7279e8c06fa7',
+            'X-RapidAPI-Host': 'tripadvisor16.p.rapidapi.com'
+          }
+        };
+      // }catch (error) {
+      //   console.error("Error", error);
+      // }
+      const second = await axios.request(restaurantResponse);
+      console.log("the second api request worked");
+      console.log(second);
+      console.log(second.data.data.data[0])
+    
+      //  if (second.data && second.data.data && Array.isArray(second.data.data)) {
+        const restaurant = second.data.data.data[0];
+       console.log(restaurant);
+
+        const formattedRestaurants = [{
+           id: restaurant.locationId,
+           title: restaurant.name,
+          rating: restaurant.averageRating|| null,
+          price: restaurant.priceTag|| null,
+        }];
+        console.log("i got here");
+        setRestaurants(formattedRestaurants);
+    //  }
+    } 
+  
+  
+  
+  
   return (
+    <div>
     <div className="restaurants-bg">
       <h1>Where would you like to eat?</h1>
       <div>
@@ -91,14 +101,11 @@ export default function Restaurants() {
           <div key={index} className="restaurant-card">
             <h3>{restaurant.title}</h3>
             <p>Rating: {restaurant.rating}</p>
-            <p>Provider: {restaurant.provider}</p>
+
             <p>Price: {restaurant.price}</p>
-            {restaurant.originalPrice && (
-              <p>Original Price: {restaurant.originalPrice}</p>
-            )}
-            <a href={restaurant.externalUrl} target="_blank">
+
               Reserve Now, Pay Later
-            </a>
+      
           </div>
         ))}
       </div>
@@ -110,30 +117,31 @@ export default function Restaurants() {
           id="query"
           name="query"
           value={consult}
-          onChange={(e) => setConsult(e.target.value)}
+        
+          onChange={handleChange}
+
+          // onChange={(e) =>{ 
+          //   console.log(e.target.value)
+          //   // setConsult(`${e.target.value}`)
+          //   setConsult(e.target.value)
+          //   console.log(consult)
+          // } 
+        // }
         />
 
-        <label className="label" htmlFor="checkIn">Check-in:</label>
+        <label className="label" htmlFor="book">Book:</label>
         <input
           type="date"
-          id="checkIn"
-          name="checkIn"
-          value={checkIn}
-          onChange={(e) => setCheckIn(e.target.value)}
-        />
-
-        <label className="label" htmlFor="checkOut">Check-out:</label>
-        <input
-          type="date"
-          id="checkOut"
-          name="checkOut"
-          value={checkOut}
-          onChange={(e) => setCheckOut(e.target.value)}
+          id="book"
+          name="book"
+          value={book}
+          onChange={(e) => setBook(e.target.value)}
         />
         <button className="search-btn" type="button" onClick={handleSearchRestaurants}>
           Reserve Now
         </button>
       </form>
     </div>
+    </div>
   );
-}
+        }
