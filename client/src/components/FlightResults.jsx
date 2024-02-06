5c72d2c67187e33930b2e78a387ca4c734757a5e
Ioanna Papaioannou <ipapioani@gmail.com>
2024-02-06 22:23:36 +0100

    book button fixed and working

--- client/src/components/FlightResults.jsx
+++ client/src/components/FlightResults.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons"; // Import the heart icon for favourites
 
@@ -14,7 +14,10 @@ export default function FlightResults({ results }) {
     };
 
     try {
-      await axios.post("http://localhost:4000/api/favourites_list", flightDetails);
+      await axios.post(
+        "http://localhost:4000/api/favourites_list",
+        flightDetails
+      );
       console.log("Flight added to favourites successfully");
     } catch (err) {
       console.error("Error adding to favourites:", err);
@@ -27,54 +30,97 @@ export default function FlightResults({ results }) {
         <div>
           <h2>Flight Search Results</h2>
           {/* Render the flight search results here */}
-          {results.data.flights.map((flight, index) => (
-            <div className="card mb-3" key={index}>
-              <div className="card-body">
-                <h5 className="card-title">Option {index + 1}</h5>
-                {/* Map over each segment within the flight */}
-                {flight.segments.map((segment, segmentIndex) =>
-                  segmentIndex === 0 ||
-                  segment.legs[0].departureDateTime !==
-                    flight.segments[segmentIndex - 1].legs[0]
-                      .departureDateTime ||
-                  segment.legs[0].arrivalDateTime !==
-                    flight.segments[segmentIndex - 1].legs[0].arrivalDateTime ||
-                  segment.legs[0].marketingCarrier.displayName !==
-                    flight.segments[segmentIndex - 1].legs[0].marketingCarrier
-                      .displayName ? (
-                    <div className="card mb-3" key={segmentIndex}>
-                      <div className="card-body">
-                        <p className="card-text">
-                          Departure airport: {segment.legs[0].originStationCode}
-                        </p>
-                        <p className="card-text">
-                          Landing Airport:{" "}
-                          {segment.legs[0].destinationStationCode}
-                        </p>
-                        <p className="card-text">
-                          Arrival: {segment.legs[0].arrivalDateTime}
-                        </p>
-                        <p className="card-text">
-                          Airline:{" "}
-                          {segment.legs[0].marketingCarrier.displayName}
-                        </p>
-                        <button onClick={() => deleteCb(segment.legs[0].id)}>
-                          delete
-                        </button>
-                        <button onClick={() => addCb(segment.legs[0].id)}>
-                          add
-                        </button>
-                        <button onClick={() => handleAddToFavourites(segment)}
-                          className="favourite-btn">
-                        <FontAwesomeIcon icon={faHeart} />
-                        </button>
+          {results.data.flights.map((flight, index) => {
+            // find the lowest price for each flight option
+            const lowestPrice = Math.min.apply(
+              null,
+              flight.purchaseLinks.map((link) => link.totalPricePerPassenger)
+            );
+            const lowestTotalPrice = Math.min.apply(
+              null,
+              flight.purchaseLinks.map((link) => link.totalPrice)
+            );
+            const lowestPriceUrl = flight.purchaseLinks.map((link) => link.url);
+
+            return (
+              <div className="card mb-3" key={index}>
+                <div className="card-body">
+                  <h5 className="card-title">Option {index + 1}</h5>
+                  {/* Map over each segment within the flight */}
+                  {flight.segments.map((segment, segmentIndex) =>
+                    segmentIndex === 0 ||
+                    segment.legs[0].departureDateTime !==
+                      flight.segments[segmentIndex - 1].legs[0]
+                        .departureDateTime ||
+                    segment.legs[0].arrivalDateTime !==
+                      flight.segments[segmentIndex - 1].legs[0]
+                        .arrivalDateTime ||
+                    segment.legs[0].marketingCarrier.displayName !==
+                      flight.segments[segmentIndex - 1].legs[0].marketingCarrier
+                        .displayName ? (
+                      <div className="card mb-3" key={segmentIndex}>
+                        <div className="card-body">
+                          <p className="card-text">
+                            Departure airport:{" "}
+                            {segment.legs[0].originStationCode}
+                          </p>
+                          <p className="card-text">
+                            Landing Airport:{" "}
+                            {segment.legs[0].destinationStationCode}
+                          </p>
+                          <p className="card-text">
+                            Arrival: {segment.legs[0].arrivalDateTime}
+                          </p>
+                          <p className="card-text">
+                            Airline:{" "}
+                            {segment.legs[0].marketingCarrier.displayName}
+                          </p>
+                          <p className="card-text">
+                            Per passenger: {lowestPrice}
+                          </p>
+                          <p className="card-text">
+                            Lowest total price: {lowestTotalPrice}
+                          </p>
+                          <button onClick={() => deleteCb(segment.legs[0].id)}>
+                            delete
+                          </button>
+                          <button
+                            onClick={() => handleAddToFavourites(segment)}
+                            className="favourite-btn"
+                          >
+                            <FontAwesomeIcon icon={faHeart} />
+                          </button>
+                        </div>
                       </div>
+                    ) : null
+                  )}
+                  {/* Add the Book buttons for each purchase link */}
+                  {/* {flight.purchaseLinks.map((purchaseLink, purchaseIndex) => (
+                    <div key={`purchase_${purchaseIndex}`}>
+                      <a
+                        href={purchaseLink.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="btn btn-primary"
+                      >
+                        Book
+                      </a>
                     </div>
-                  ) : null
-                )}
+                  ))} */}
+                  <div>
+                    <a
+                      href={lowestPriceUrl}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="btn btn-primary"
+                    >
+                      Book
+                    </a>
+                  </div>
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       )}
     </div>
